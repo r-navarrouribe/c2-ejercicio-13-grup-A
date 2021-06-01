d520777dd48b7d59784f0947177a769dc1ac8573
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-01 20:15:44 +0200

    Bugfix del querySelector de los clones

--- js/index.js
+++ js/index.js
@@ -9,6 +9,8 @@ const getfacturasIngreso = (tipoFactura) =>
 
 const facturasIngreso = getfacturasIngreso("ingreso");
 
+const contenedorFilas = document.querySelector(".contenedor-filas");
+
 // Iteración de facturas tipo ingreso
 
 for (const {
@@ -24,39 +26,40 @@ for (const {
   const filaElemento = document.querySelector(".fila-molde").cloneNode(true);
   filaElemento.classList.remove("fila-molde");
 
+  console.log(filaElemento);
+
   // Número
-  const numeroFactura = document.querySelector(".numero");
+  const numeroFactura = filaElemento.querySelector(".numero");
   numeroFactura.textContent = numero;
 
   // Fecha
 
-  const fechaFactura = document.querySelector(".fecha");
+  const fechaFactura = filaElemento.querySelector(".fecha");
   const pasarFecha = new Date(fecha);
   fechaFactura.textContent = pasarFecha.toLocaleDateString();
 
   // Vencimiento
 
-  const vencimientoFactura = document.querySelector(".vence");
+  const vencimientoFactura = filaElemento.querySelector(".vence");
   const pasarFechaVencimiento = new Date(vencimiento);
   vencimientoFactura.textContent = pasarFechaVencimiento.toLocaleDateString();
 
   // Concepto
 
-  const conceptoFactura = document.querySelector(".concepto");
+  const conceptoFactura = filaElemento.querySelector(".concepto");
   conceptoFactura.textContent = concepto;
 
   // Base
 
-  const baseFactura = document.querySelector(".base");
+  const baseFactura = filaElemento.querySelector(".base");
   baseFactura.textContent = base;
 
   // Iva
 
-  const ivaFactura = document.querySelector(".iva");
+  const ivaFactura = filaElemento.querySelector(".iva");
   const calculoBaseMasIva = (base * tipoIva) / 100;
   ivaFactura.textContent = `${calculoBaseMasIva}€ (${tipoIva}%)`;
 
   // Añadir filas
-  const contenedorFilas = document.querySelector(".contenedor-filas");
   contenedorFilas.append(filaElemento);
 }
